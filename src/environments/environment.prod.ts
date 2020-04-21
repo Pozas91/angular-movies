16afc12a675f81f6c7944412a78b3e92069582b4
Pozas91 <pozas91@gmail.com>
2020-04-21 18:50:50 +0200

    GitHub Pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  firebaseAPIKey: 'AIzaSyCLTMZwpHukotloBhAQIqR0EHOx6a8Si1c'
+  firebaseAPIKey: 'AIzaSyCLTMZwpHukotloBhAQIqR0EHOx6a8Si1c',
+  baseUrlApi: 'https://compare-spa-apps.firebaseio.com/'
 };
